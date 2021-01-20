f51facbed3e2a8ebf89f1233cba8bc766c28056b
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-20 17:48:00 +0530

    WPF-56496 - Spanned appointment sample added.

---
+++ SchedulerWPF/SchedulerWPF/Model/Meeting.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class Meeting
+    {
+        public string EventName { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public Brush BackgroundColor { get; set; }
+        public Brush ForegroundColor { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class SchedulerViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Meeting> meetings;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public SchedulerViewModel()
+        {
+            this.Meetings = new ObservableCollection<Meeting>();
+            this.AddAppointments();
+
+        }
+        public ObservableCollection<Meeting> Meetings
+        {
+            get
+            {
+                return this.meetings;
+            }
+            set
+            {
+                this.meetings = value;
+                this.RaiseOnPropertyChanged("Meetings");
+            }
+        }
+        private void AddAppointments()
+        {
+            var meeting = new Meeting();
+            meeting.EventName = "Meeting";
+            meeting.From = new DateTime(2020, 12, 15, 10, 0, 0);
+            meeting.To = meeting.From.AddDays(2).AddHours(1);
+            meeting.BackgroundColor = new SolidColorBrush(Colors.SlateBlue);
+            meeting.ForegroundColor = new SolidColorBrush(Colors.White);
+            this.Meetings.Add(meeting);
+        }
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+    
+
